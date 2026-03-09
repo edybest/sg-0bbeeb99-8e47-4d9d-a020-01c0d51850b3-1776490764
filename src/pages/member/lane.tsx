2847bef422c5da65e941f47b5c7360c091b4e000
Softgen Dev <dev@softgen.ai>
2026-03-09 01:29:26 +0000

    Updates

--- src/pages/member/lane.tsx
+++ src/pages/member/lane.tsx
@@ -55,6 +55,7 @@ export default function LanePage() {
   useEffect(() => {
     if (selectedGameId) {
       loadLaneAssignments();
+      loadSpinResults(selectedGameId);
     }
   }, [selectedGameId]);
 
